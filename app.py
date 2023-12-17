2ee3059240c4877a4ec7dc3fd6c5220f4ea5695c
Tianlang(Harris) Gu <123590079+tianlangS2023@users.noreply.github.com>
2023-12-16 16:48:31 -0800

    added .gitignore

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response,request,jsonify
+from flask import Flask, render_template, Response, request, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO
 import cv2
@@ -10,82 +10,98 @@ import json
 import threading
 import time
 
+# Initialize Flask app
 app = Flask(__name__)
+# Initialize SocketIO with CORS allowed from any origin, and enable logs
 socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
-#CORS(app)
 
-capture = False  # Global flag to control capture state
-FPS = 5 # default Frames Per Second capture rate
-frame_thread = None
+# Global variables to control the capture process and frame rate
+capture = False  # Flag to control the capture process
+FPS = 5  # Default Frames Per Second capture rate
+frame_thread = None  # Thread for capturing frames
 
+# Function to capture frames from the default camera
 def capture_frames():
+    # Initialize video capture
     cap = cv2.VideoCapture(0)
-    frame_count = 0
+    frame_count = 0  # Initialize frame counter
+
+    # Loop to capture frames as long as 'capture' is True
     while capture:
+        # Capture a single frame
         ret, frame = cap.read()
+        # If frame capture fails, break loop
         if not ret:
             break
 
+        # Encode the frame in JPEG format
         ret, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
-        #encoded_frame = base64.b64encode(frame_bytes).decode('utf-8')
 
+        # Get current timestamp
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        # Log frame capture with timestamp
         with open("frame_log.txt", "a") as log_file:
             log_file.write(f"Frame {frame_count} captured at {timestamp}\n")
             log_file.write(f"frame_bytes at {timestamp}: {frame_bytes}\n\n")
+        # Emit the captured frame to all connected SocketIO clients
         socketio.emit('stream_frame', {'frame': frame_bytes, 'timestamp': timestamp})
+        # Wait for a while before capturing the next frame
         socketio.sleep(1.0 / FPS)
-        frame_count += 1
+        frame_count += 1  # Increment frame counter
 
+    # Release video capture when loop ends
     cap.release()
 
+# Flask before_request handler to log the headers of incoming requests
 @app.before_request
 def before_request():
     print(request.headers)
     data = request.get_data(as_text=True)
     print("the whole request: ", data)
 
+# Route to serve the video streaming page
 @app.route('/')
 def index():
-    # The HTML page that will display the video stream
     return render_template('index.html')
 
+# Route to handle starting the capture process
 @app.route('/start_capture', methods=['POST'])
 def start_capture():
-    global capture, frame_thread, FPS  # Declare FPS as global here
-    data = request.get_json()
-    FPS = float(data.get('fps'))
-    capture = True
+    global capture, frame_thread, FPS
+    data = request.get_json()  # Get data from the incoming request
+    FPS = float(data.get('fps'))  # Update FPS based on client request
+    capture = True  # Set capture flag to True to start frame capturing
+    # Start a new thread for capturing frames
     threading.Thread(target=capture_frames, daemon=True).start()
-
-
-    # if frame_thread is None or not frame_thread.is_alive():
-    #     frame_thread = threading.Thread(target=capture_frames)
-    #     frame_thread.start()
     print("capture started")
+    # Respond to the client that capture has started
     return jsonify({"message": "Capture started"})
 
-
+# Route to handle stopping the capture process
 @app.route('/stop_capture', methods=['POST'])
 def stop_capture():
     global capture
-    capture = False
+    capture = False  # Set capture flag to False to stop frame capturing
     print("capture stopped")
+    # Respond to the client that capture has stopped
     return jsonify({"message": "Capture stopped"})
 
+# SocketIO event handler for starting the stream
 @socketio.on('start_stream')
 def handle_start_stream(message):
-    print("in start stream")
     global capture
-    capture = True
+    capture = True  # Set capture flag to True to start frame capturing
+    # Start a new thread for capturing frames
     threading.Thread(target=capture_frames, daemon=True).start()
+    print("in start stream")
 
+# SocketIO event handler for stopping the stream
 @socketio.on('stop_stream')
 def handle_stop_stream(message):
     global capture
-    capture = False
+    capture = False  # Set capture flag to False to stop frame capturing
 
+# Main entry point for running the app
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
-
+    socketio.run(app, debug=True)  # Run the app with debug mode on
